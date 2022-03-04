6bc9081ba3182e966ab64b71d4f8f1136ae8b000
Animesh373 <88077067+Animesh373@users.noreply.github.com>
2022-03-04 21:56:41 +0530

    added 2. BSRecursive.cpp

---
+++ 1. Binary Search/2. BSRecursive.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int BSRecursive(int arr[] ,  int x , int low , int high)
+{
+	if (low <= high)
+	{
+		int mid = (low + high) / 2;
+		if (arr[mid] == x) return mid;
+		else if (arr[mid] > x) return BSRecursive (arr , x , low , mid - 1);
+		else if (arr[mid] < x) return BSRecursive (arr , x , mid + 1 , high);
+	}
+	return -1;
+}
+
+int main()
+{
+	int n , x;
+	cin >> n;
+	int arr[n];
+	for (int i = 0; i < n; ++i) cin >> arr[i];
+	cin >> x;
+	int ans = BSRecursive(arr , x , 0 , n - 1);
+	cout << ans;
+}
+
+// I/P -> size_of_array , Array , element_to_search
+// O/P -> index_of_element
